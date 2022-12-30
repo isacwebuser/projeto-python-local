4a7659be1803ce32bc17cbf9794deb91d6048839
Isac Souza <isacrgs@gmail.com.br>
2022-12-30 08:27:38 -0300

    adicionando atributo category e executanto migrate

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe(models.Model):
@@ -16,3 +21,6 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to="recipes/covers/%Y/%m/%d/%H/",
                               height_field=None, width_field=None, max_length=None)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
