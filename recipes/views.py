5ad4d6fd5152fa2eb730bce4d6620a4381065c8c
Isac Souza <isacrgs@gmail.com.br>
2023-01-06 14:02:07 -0300

    ajustes finais views templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from utils.recipes.factory import make_recipe
 
@@ -9,23 +9,29 @@ from .models import Recipe
 
 
 def my_home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 
 def recipe(request, id):
+
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'recipe_detail': True,
     })
 
 
 def category(request, category_id):
-    category = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
+
+    category = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': category,
-        'title': f' | Category - {category.first().category.name}'
+        'title': f' | Category - {category[0].category.name}'
     })
